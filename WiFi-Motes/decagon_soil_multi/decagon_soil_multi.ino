ed39521570850dfae11a471d759ee9a5de865b56
noblek <noblek@oregonstate.edu>
2018-02-16 15:48:56 -0800

    Added 32u4 compatibility

--- WiFi-Motes/decagon_soil_multi/decagon_soil_multi.ino
+++ WiFi-Motes/decagon_soil_multi/decagon_soil_multi.ino
@@ -5,13 +5,14 @@ Decagon shield polling functionality demo
 #include "SDI12.h"
 #include <string.h>
 #include "SensorList.h"
+#include <EnableInterrupt.h>
 
-#define DATAPIN1 A0  // change to the proper pin
-#define DATAPIN2 A1
-#define DATAPIN3 A2
-#define DATAPIN4 A3
-#define DATAPIN5 A4
-#define DATAPIN6 A5
+#define DATAPIN1 8  // change to the proper pin
+#define DATAPIN2 9
+#define DATAPIN3 10
+#define DATAPIN4 11
+#define DATAPIN5 14
+#define DATAPIN6 15
 #define SENSOR_ADDRESS "?"
 
 //Declare 6 SDI-12 objects initialized with DATAPIN1-6
@@ -21,11 +22,6 @@ SDI12 mySDI12[6] = {DATAPIN1, DATAPIN2, DATAPIN3, DATAPIN4, DATAPIN5, DATAPIN6};
 struct SensorList data;
 
 String sdiResponse = "";
-char buf[20];
-char *p = buf;
-float dielec_p = 0;
-float temp = 0;
-float elec_c = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -35,6 +31,21 @@ void setup() {
     mySDI12[i].begin();
     mySDI12[i].forceHold();
   }
+
+#ifdef SDI12_EXTERNAL_PCINT
+  pinMode(DATAPIN1, INPUT_PULLUP);
+  enableInterrupt(DATAPIN1, SDI12::handleInterrupt, CHANGE);
+  pinMode(DATAPIN2, INPUT_PULLUP);
+  enableInterrupt(DATAPIN2, SDI12::handleInterrupt, CHANGE);
+  pinMode(DATAPIN3, INPUT_PULLUP);
+  enableInterrupt(DATAPIN3, SDI12::handleInterrupt, CHANGE);
+  pinMode(DATAPIN4, INPUT_PULLUP);
+  enableInterrupt(DATAPIN4, SDI12::handleInterrupt, CHANGE);
+  pinMode(DATAPIN5, INPUT_PULLUP);
+  enableInterrupt(DATAPIN5, SDI12::handleInterrupt, CHANGE);
+  pinMode(DATAPIN6, INPUT_PULLUP);
+  enableInterrupt(DATAPIN6, SDI12::handleInterrupt, CHANGE);
+#endif
 }
 
 void loop() {
